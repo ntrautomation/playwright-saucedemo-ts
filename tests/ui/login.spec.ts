b35d5c5c91c9d5d15428f5801fe005c3a37befd7
Nenad Trajanovski <nenad.trajanovski@scalefocus.com>
2024-07-03 16:10:05 +0200

    moving credentials and urls behind .env

--- tests/ui/login.spec.ts
+++ tests/ui/login.spec.ts
@@ -2,9 +2,9 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Login page test', () => {
     test('Login - Happy flow', async ({ page }) => {
-        await page.goto('https://www.saucedemo.com/');
-        await page.locator('[data-test="username"]').fill('standard_user');
-        await page.locator('[data-test="password"]').fill('secret_sauce');
+        await page.goto(process.env.BASE_URL);
+        await page.locator('[data-test="username"]').fill(process.env.USER_NAME);
+        await page.locator('[data-test="password"]').fill(process.env.PASSWORD);
         await page.locator('[data-test="login-button"]').click();
         expect(1).toBe(1);
       }); 
