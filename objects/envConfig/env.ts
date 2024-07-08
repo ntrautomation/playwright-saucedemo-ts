ecc47ceabba672932f9ac6e083cfd75425b07d13
Nenad Trajanovski <nenad.trajanovski@scalefocus.com>
2024-07-08 14:19:17 +0200

    adding env config

---
+++ objects/envConfig/env.ts
@@ -0,0 +1,3 @@
+export class Env {
+    public static readonly INV_URL: string = `${process.env.BASE_URL}inventory.html`
+}
--- tests/ui/home.spec.ts
+++ tests/ui/home.spec.ts
@@ -1,18 +1,19 @@
 import { test, expect } from '@playwright/test';
 import { BurgerMenuItems, ProductItems } from '@helpers/constants/burgerMenuItems';
 import HomePage from "@pages/home.page";
+import { Env } from '@envConfig/env';
 
 test.describe('Home page tests', () => {
     let homePage : HomePage;
 
     test.beforeEach(async ({ page }) => {
         homePage = new HomePage(page);
-        await homePage.navigate(process.env.INV_URL)
+        await homePage.navigate(Env.INV_URL)
     })
 
     test('Validate burger menu items', async () => {
         await homePage.clickOnBurgerMenu()
-        expect(await homePage.getBurgerMenuItems()).toEqual(BurgerMenuItems)  
+        expect(await homePage.getBurgerMenuItems()).toEqual(BurgerMenuItems) 
     });
 
     test('Validate product items', async () => {
